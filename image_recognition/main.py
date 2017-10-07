496b175bcd431b006307c338488e7172dbd32c17
Barthilas <snetivy.martin@seznam.cz>
2017-10-07 11:51:19 +0200

    Mostly done

--- image_recognition/main.py
+++ image_recognition/main.py
@@ -1,3 +1,17 @@
 import numpy as nm
 import cv2
 import matplotlib
+
+#0 grayscale, 1 colored
+img = cv2.imread('img/kitten.jpg',1)
+cv2.imshow('cat',img)
+height, width, channel = img.shape; #delete channel if grayscale
+
+for x in range(width):
+    for y in range(height):
+        #Do manual grayscale
+        img[y,x]=10;
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()

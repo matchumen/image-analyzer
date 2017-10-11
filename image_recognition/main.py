cdcd30410c8bfba10cb8b014686433b8bb16c3d3
Barthilas <snetivy.martin@seznam.cz>
2017-10-11 17:02:00 +0200

    test

--- image_recognition/main.py
+++ image_recognition/main.py
@@ -13,7 +13,7 @@ for i in range(width):
         #img[y,x]=155;
         img[j,i] = img[j,i,0] * 0.3 + img[j,i,1] * 0.59 + img[j,i,2] * 0.11
         #print(img[y,x])
-
+        ddsadas
 cv2.imshow('cat2',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

7b13c071310a08a17d086c324d1a312ea68d4802
Barthilas <snetivy.martin@seznam.cz>
2017-10-11 18:20:59 +0200

    Numpy, matplotlib, histogram

--- image_recognition/main.py
+++ image_recognition/main.py
@@ -1,19 +1,39 @@
-import numpy as nm
 import cv2
-import matplotlib
+import numpy as np
+from matplotlib import pyplot as plt
 
-#0 grayscale, 1 colored
-img = cv2.imread('img/kitten.jpg',1)
-cv2.imshow('cat',img)
-height, width, channel = img.shape; #delete channel if grayscale
+def histogram(grayscale_image):
+    """Custom histogram"""
+    hist = np.zeros(256)
+    height, width, channel = grayscale_image.shape
+    for i in range(width):
+        for j in range(height):
+            intensity = grayscale_image[j,i,0]
+            hist[intensity] += 1
+    plt.title("Histogram")
+    plt.xlabel("Intensity")
+    plt.ylabel("Amount")
+    plt.plot(hist)
+    plt.show()
 
-for i in range(width):
-    for j in range(height):
-        #Do manual grayscale
-        #img[y,x]=155;
-        img[j,i] = img[j,i,0] * 0.3 + img[j,i,1] * 0.59 + img[j,i,2] * 0.11
-        #print(img[y,x])
-        ddsadas
-cv2.imshow('cat2',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def custom_grayscale(img):
+    """Custom grayscale"""
+    height, width, channel = img.shape; #delete channel if grayscale
+    for i in range(width):
+        for j in range(height):
+            #img[y,x]=155;
+            img[j,i] = img[j,i,0] * 0.3 + img[j,i,1] * 0.59 + img[j,i,2] * 0.11
+
+    return img
+
+def main():
+    img = cv2.imread('img/kitten.jpg',1)#0 grayscale, 1 colored
+    cv2.imshow('cat',img)
+    img_grayscale = custom_grayscale(img)
+    cv2.imshow('cat2',img_grayscale)
+    histogram(img_grayscale)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()

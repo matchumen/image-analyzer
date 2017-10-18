60dd331c5a6fc249d1c44968a6097b142f8100c7
Barthilas <snetivy.martin@seznam.cz>
2017-10-18 21:29:50 +0200

    Hokus pokus, YCBCV test

--- image_recognition/main.py
+++ image_recognition/main.py
@@ -32,13 +32,47 @@ def calcHistExample(grayscale_image):
     plt.plot(hist)
     plt.show()
 
+def colorSpace_YCbCv(img):
+    """custom implementation of CVTCOLOR"""
+    height, width, channel = img.shape
+    img_YCRCB = img.copy();
+    for i in range(width):
+        for j in range(height):
+            img_YCRCB[j,i,0] = ((img_YCRCB[j,i,0]*0.257)+16) + ((img_YCRCB[j,i,1]*0.504) + 128) + ((img_YCRCB[j,i,2]*0.098)+128)
+            img_YCRCB[j,i,1] = ((img_YCRCB[j,i,0]*-0.148)+16) + ((img_YCRCB[j,i,1]*-0.291) + 128) + ((img_YCRCB[j,i,2]*0.439)+128)
+            img_YCRCB[j,i,2] = ((img_YCRCB[j,i,0]*0.439)+16) + ((img_YCRCB[j,i,1]*-0.368) + 128) + ((img_YCRCB[j,i,2]*-0.071)+128)
+    return img_YCRCB
+
+def showChannel(img, channel):
+    x = img.copy()
+    if(channel==0):
+        x[:, :, 1] = 0
+        x[:, :, 2] = 0
+    if(channel==1):
+        x[:, :, 0] = 0
+        x[:, :, 2] = 0
+    if(channel==2):
+        x[:, :, 0] = 0
+        x[:, :, 1] = 0
+    return x
+
+
 def main():
-    img = cv2.imread('img/kitten.jpg',1)#0 grayscale, 1 colored
-    #cv2.imshow('cat',img)
+    obrazek = 'img/kostky.png'
+    img = cv2.imread(obrazek,1)#0 grayscale, 1 colored
+
+    """
+    cv2.imshow('cat',img)
     img_grayscale = custom_grayscale(img)
-    #cv2.imshow('cat2',img_grayscale)
-    #histogram(img_grayscale)
-    #calcHistExample(img_grayscale)
+    cv2.imshow('cat2',img_grayscale)
+    histogram(img_grayscale)
+    calcHistExample(img_grayscale)5
+    """
+    #img_YCbCv = colorSpace_YCbCv(img)
+    img_2 = cv2.cvtColor(img, cv2.COLOR_RGB2YCR_CB)
+    #cv2.imshow('custom', img_YCbCv)
+    cv2.imshow('inbuilt', showChannel(img_2, 2))
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
